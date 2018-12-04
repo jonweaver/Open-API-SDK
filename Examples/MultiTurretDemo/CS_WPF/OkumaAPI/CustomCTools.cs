873709476dc46e456bfb0b5e889d39244bddb1f8
GOSIGER\jweaver <jweaver@gosiger.com>
2018-12-04 15:21:37 -0500

    Adding multi turret demo app

---
+++ Examples/MultiTurretDemo/CS_WPF/OkumaAPI/CustomCTools.cs
@@ -0,0 +1,61 @@
+﻿using Okuma.CLDATAPI.DataAPI;
+using Okuma.CLDATAPI.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_WPF.OkumaAPI
+{
+
+    public class CustomCTools : CTools
+    {
+
+        public CustomCTools(SubSystemEnum subSystem) : base(subSystem)
+        {
+            SubSystem = subSystem;
+        }
+
+        public new void SetSubSystem(SubSystemEnum subSystem)
+        {
+            SubSystem = subSystem;
+            base.SetSubSystem(subSystem);
+        }
+
+        public new void SetDataUnit(DataUnitEnum unit)
+        {
+            DataUnit = unit;
+            base.SetDataUnit(unit);
+        }
+
+        public SubSystemEnum SubSystem { get; internal set; }
+        public DataUnitEnum DataUnit { get; internal set; }
+    }
+
+    public class CustomCTurret :CTurret
+    {
+        public CustomCTurret(SubSystemEnum subSystem) : base(subSystem)
+        {
+            SubSystem = subSystem;
+        }
+        public new void SetSubSystem(SubSystemEnum subSystem)
+        {
+            SubSystem = subSystem;
+            base.SetSubSystem(subSystem);
+        }
+
+        public new void SetDataUnit(DataUnitEnum unit)
+        {
+            DataUnit = unit;
+            base.SetDataUnit(unit);
+        }
+
+        public SubSystemEnum SubSystem { get; internal set; }
+        public DataUnitEnum DataUnit { get; internal set; }
+
+
+    }
+  
+}
---
+++ Examples/MultiTurretDemo/CS_WPF/OkumaAPI/OkumaLathe.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataApi = Okuma.CLDATAPI.DataAPI;
+using DataEnums = Okuma.CLDATAPI.Enumerations;
+
+namespace CS_WPF.OkumaAPI
+{
+    public interface IOkumaLathe
+    {
+        string GetToolComment(int regNum, DataEnums.TurretEnum turret);
+        void SetToolComment(int regNum, DataEnums.TurretEnum turret, string toolComment);
+    }
+    public class OkumaLathe : IDisposable, IOkumaLathe
+    {
+
+
+        private Dictionary<DataEnums.TurretEnum, DataApi.CTools> _toolObjDict;
+        private DataApi.CMachine _machine;
+
+
+        public OkumaLathe()
+        {
+
+            _machine = new DataApi.CMachine();
+            // 'Init()' must be called exactly once on the main
+            //   thread from an instance of CMachine before any
+            //   API operations can take place.
+            // Note that the instance of CMachine need not remain
+            //   after 'Init()' is called as that part of CMachine
+            //   is actually static.
+            _machine.Init();
+
+            //The _toolObjDict is a dictionary that will hold rerences to the different instances of the CTools class
+            //keyed by the turret type
+
+            _toolObjDict = new Dictionary<DataEnums.TurretEnum, DataApi.CTools>();
+
+        }
+
+
+        public string GetToolComment(int regNum, DataEnums.TurretEnum turret)
+        {
+            var cTools = GetToolsObj(turret);
+            return cTools.GetToolComment(regNum);
+        }
+
+        public void SetToolComment(int regNum, DataEnums.TurretEnum turret, string toolComment)
+        {
+            var cTools = GetToolsObj(turret);
+            cTools.SetToolComment(regNum, toolComment);
+        }
+
+        private DataApi.CTools GetToolsObj(DataEnums.TurretEnum turret)
+        {
+            //When an instance of CTools is requested check the dictionary and if has not already been instantiated
+            //create it and add it to the dictionary
+            if (!_toolObjDict.ContainsKey(turret))
+            {
+                _toolObjDict.Add(turret, new CustomCTools(turret == DataEnums.TurretEnum.A_Turret
+                                                          ? DataEnums.SubSystemEnum.NC_AL
+                                                          : DataEnums.SubSystemEnum.NC_BL));
+            }
+
+            return _toolObjDict[turret];
+        }
+
+        public void Dispose()
+        {
+            if (_machine != null)
+            {
+                _machine.Close();
+                _toolObjDict.Clear();
+            }
+        }
+    }
+}
